5e16af7a062763d47a7a2c4400a154c5507355c4
Alexis Perrier <alexis.perrier@praexo.com>
2021-11-14 14:32:16 +0100

    load model

--- sandbox/transfer.py
+++ sandbox/transfer.py
@@ -7,3 +7,16 @@ import subprocess
 import torch
 import time
 import tqdm
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from style_paraphrase.inference_utils import GPT2Generator
+
+# from profanity_filter import ProfanityFilter
+OUTPUT_DIR = "./data/"
+if __name__ == "__main__":
+
+    with torch.cuda.device(0):
+        print("Loading Tweets model...")
+        tweets = GPT2Generator(OUTPUT_DIR + "/models/cds_models/tweets")
